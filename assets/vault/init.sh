2f1a376ac3b06498f28f24451ded0595537f6234
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2024-07-02 19:17:04 +0200

    fix: fix vault key retrieval

--- assets/vault/init.sh
+++ assets/vault/init.sh
@@ -16,36 +16,35 @@ done
 
 
 # initialize vault
-echo "Initializing vault..."
-sudo docker exec vault /bin/sh -c "vault operator init --address '${ADDRESS}'" > /tmp/vault-init.txt
-
+if [[ -f /opt/vault-init.txt ]]; then
+    echo "Vault already initialized. Skipping..."
+else
+    echo "Initializing vault..."
+    sudo docker exec vault /bin/sh -c "vault operator init --address '${ADDRESS}'" > /opt/vault-init.txt
+fi
 
 # check if vault was already initialized or not
 if [[ $? -eq 0 ]]; then
     # parse token and recovery keys
     echo "Vault initialized successfully. Parsing keys..."
-    VAULT_TOKEN=$(sudo grep 'Initial Root Token:' /tmp/vault-init.txt | awk '{print $4}')
-    VAULT_RECOVERY_KEY_1=$(sudo grep 'Recovery Key 1:' /tmp/vault-init.txt | awk '{print $4}')
-    VAULT_RECOVERY_KEY_2=$(sudo grep 'Recovery Key 2:' /tmp/vault-init.txt | awk '{print $4}')
-    VAULT_RECOVERY_KEY_3=$(sudo grep 'Recovery Key 3:' /tmp/vault-init.txt | awk '{print $4}')
-    VAULT_RECOVERY_KEY_4=$(sudo grep 'Recovery Key 4:' /tmp/vault-init.txt | awk '{print $4}')
-    VAULT_RECOVERY_KEY_5=$(sudo grep 'Recovery Key 5:' /tmp/vault-init.txt | awk '{print $4}')
+    VAULT_TOKEN=$(sudo grep 'Initial Root Token:' /opt/vault-init.txt | awk '{print $4}')
+    VAULT_RECOVERY_KEY_1=$(sudo grep 'Recovery Key 1:' /opt/vault-init.txt | awk '{print $4}')
+    VAULT_RECOVERY_KEY_2=$(sudo grep 'Recovery Key 2:' /opt/vault-init.txt | awk '{print $4}')
+    VAULT_RECOVERY_KEY_3=$(sudo grep 'Recovery Key 3:' /opt/vault-init.txt | awk '{print $4}')
+    VAULT_RECOVERY_KEY_4=$(sudo grep 'Recovery Key 4:' /opt/vault-init.txt | awk '{print $4}')
+    VAULT_RECOVERY_KEY_5=$(sudo grep 'Recovery Key 5:' /opt/vault-init.txt | awk '{print $4}')
 
     # output for parsing
     echo "--START TOKENS--"
     echo "---"
     echo "root_token: $VAULT_TOKEN"
-    echo "recovery_keys: $VAULT_RECOVERY_KEY_1"
+    echo "recovery_keys:"
     echo "  - ${VAULT_RECOVERY_KEY_1}"
     echo "  - ${VAULT_RECOVERY_KEY_2}"
     echo "  - ${VAULT_RECOVERY_KEY_3}"
     echo "  - ${VAULT_RECOVERY_KEY_4}"
     echo "  - ${VAULT_RECOVERY_KEY_5}"
     echo "--END TOKENS--"
-
-    # cleanup
-    echo "Cleaning up..."
-    sudo rm /tmp/vault-init.txt || true
 else
     echo "Vault already initialized. Skipping..."
 fi
